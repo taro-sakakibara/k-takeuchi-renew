64730a0715b3a082385508ba89160139ecbbb1f4
taro.sakakibara <sakakibara@monosus.co.jp>
2021-11-08 08:50:39 +0900

    fix

--- src/components/modules/index.ts
+++ src/components/modules/index.ts
@@ -1,4 +1,4 @@
 export { ContactButton } from './Button/ContactButton'
 export { OrderButton } from './Button/OrderButton'
-export { ParallaxBottom } from './ParallaxBottom'
-export { ParallaxTop } from './ParallaxTop'
+export { ParallaxBottom } from './Parallax/ParallaxBottom'
+export { ParallaxTop } from './Parallax/ParallaxTop'
