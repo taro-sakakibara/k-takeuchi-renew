b88245c84934e269f2c083277ba09f797d850901
taro.sakakibara <sakakibara@monosus.co.jp>
2021-11-16 22:26:33 +0900

    :bug: fix: fix: バーガー完成！

--- src/components/elements/header/GlobalNav.tsx
+++ src/components/elements/header/GlobalNav.tsx
@@ -1,29 +1,30 @@
 import tw, { styled } from 'twin.macro'
 import Scroll from 'react-scroll'
+import { useState } from 'react'
+import { useMedia } from 'use-media'
 
-const NavWrapper = styled.nav`
+// global nav
+const NavWrapper = styled.nav<{ isOpen: boolean }>`
   ${tw`
 right-0 text-right
-md:(
-	 bg-white pr-[35px]
-	)
+
+md:( bg-white pr-[35px])
 sp:(
 	w-[calc(690/750*100vw)]
-	fixed h-[100vh] bg-deepGray opacity-[96%]
-	z-max
+	fixed h-[100vh] bg-deepGray
+	z-gNav1 transition-[all 0.1s ease-in-out]
 )
 `}
+
+  right: ${({ isOpen }) => (isOpen ? '0' : 'calc(-690/750*100vw)')};
+  opacity: ${({ isOpen }) => (isOpen ? '96%' : '0')};
 `
 
 const ListWrapper = styled.ul`
   ${tw`
 inline-flex h-full
-md:(
-	py-[40px]
-	)
-sp:(
-	flex-col justify-center pr-[4.6vw]
-)
+md:(py-[40px])
+sp:(flex-col justify-center pr-[4.6vw])
 `}
 `
 
@@ -34,34 +35,78 @@ md:(text-baseSize text-base ml-[65px] first:ml-0)
 sp:(text-large text-white text-right mt-[calc(120/750*100vw)] first:mt-0)
 `}
 `
-export const GlobalNav: React.FC = () => {
-  const text = [
-    { title: 'Concept', scrollTo: 'concept' },
-    { title: 'Lesson', scrollTo: 'lesson' },
-    { title: 'FAQ', scrollTo: 'faq', offset: 280 },
-    { title: 'Corse', scrollTo: 'corse' },
-    { title: 'Instructor', scrollTo: 'instructor' },
-    { title: 'Contact', scrollTo: 'contact' },
-  ]
 
-  const items = text.map((v, i) => (
-    <ListItem key={i}>
-      {v.offset ? (
-        <Scroll.Link to={v.scrollTo} smooth={true} offset={v.offset}>
-          {v.title}
-        </Scroll.Link>
-      ) : (
-        <Scroll.Link to={v.scrollTo} smooth={true}>
-          {v.title}
-        </Scroll.Link>
-      )}
-    </ListItem>
-  ))
+// hamburger
+const HamburgerWrapper = styled.button`
+  ${tw`
+fixed top-[25px] right-[25px] inline-block w-[55px] h-[12px]
+cursor-pointer z-gNav2
+`}
+`
+
+const HamburgerLine = styled.span<{ isOpen: boolean }>`
+  ${tw`
+	flex flex-col justify-between w-[55px] h-[12px]
+
+	before:(block h-[1px] bg-white transition-[all 0.1s ease-in-out])
+	after:(block h-[1px] bg-white transition-[all 0.1s ease-in-out])
+	`}
+
+  :before {
+    transform: ${({ isOpen }) => isOpen && 'translateY(5px) rotate(30deg)'};
+  }
+  :after {
+    transform: ${({ isOpen }) => isOpen && 'translateY(-5px) rotate(-30deg)'};
+  }
+`
+
+const text = [
+  { title: 'Concept', scrollTo: 'concept' },
+  { title: 'Lesson', scrollTo: 'lesson' },
+  { title: 'FAQ', scrollTo: 'faq', offset: 280 },
+  { title: 'Corse', scrollTo: 'corse' },
+  { title: 'Instructor', scrollTo: 'instructor' },
+  { title: 'Contact', scrollTo: 'contact' },
+]
+
+export const GlobalNav: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const isWide = useMedia({ minWidth: '769px' })
 
   return (
-    <NavWrapper>
-      <ListWrapper>{items}</ListWrapper>
-    </NavWrapper>
+    <>
+      <NavWrapper isOpen={isOpen}>
+        <ListWrapper>
+          {text.map((v, i) => (
+            <ListItem key={i}>
+              {v.offset ? (
+                <Scroll.Link
+                  to={v.scrollTo}
+                  smooth={true}
+                  offset={v.offset}
+                  onClick={() => setIsOpen(!isOpen)}
+                >
+                  {v.title}
+                </Scroll.Link>
+              ) : (
+                <Scroll.Link
+                  to={v.scrollTo}
+                  smooth={true}
+                  onClick={() => setIsOpen(!isOpen)}
+                >
+                  {v.title}
+                </Scroll.Link>
+              )}
+            </ListItem>
+          ))}
+        </ListWrapper>
+      </NavWrapper>
+      {isWide == false && (
+        <HamburgerWrapper onClick={() => setIsOpen(!isOpen)}>
+          <HamburgerLine isOpen={isOpen} />
+        </HamburgerWrapper>
+      )}
+    </>
   )
 }
 export default GlobalNav
